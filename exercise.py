9c639e4bd5443df1662dbed7ec62793669ebd91e
Leticia Valladares <leticia.valladares@cloudreach.com>
2022-09-28 14:45:03 +0200

    convert old str format into f-format

--- exercise.py
+++ exercise.py
@@ -1,3 +1,5 @@
+from rich import print
+
 # Knowing that 5 apples equals to 1 Kg
 # Inout the amount of apples
 # Ask if the weight needs to be in (k)KG or (l)lbs
@@ -7,21 +9,21 @@ apple_g = 1000 / 5    # 200g
 
 lb = 0.002205    # 1g -> 0.002205lb
 
-apple_amount = int(input(f"Enter amount of apples: "))
+apple_amount = int(input("Enter amount of apples: "))
 
 unit = "empty"
 
 while (unit != "l" or unit != "k"):
-    unit = input(f"Do you want to know the weight in kg 'k' or lb 'l'?")
+    unit = input("Do you want to know the weight in kg 'k' or lb 'l'?")
     if unit == "l":
         result = (int(apple_amount) * apple_g) * lb
-        print(str(apple_amount) + f" apples weight " + str(result))
+        print(f"{apple_amount} apples weight {result}")
         break
     elif unit == "k":
         result = (int(apple_amount) * apple_g)
-        print(str(apple_amount) + f" apples weight " + str(result))
+        print(f"{apple_amount} apples weight {result}")
         break
     else:
-        print(f"Please enter 'k' or 'l'")
+        print("Please enter 'k' or 'l'")
 
 
